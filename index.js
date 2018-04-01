1af06d3fcafc6736acbf5fbe75879718c0081091
Morgan Fudge <mfudge92@gmail.com>
2018-04-01 21:18:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function getFirstSelector(s) {
   return document.getElementById('app').querySelectorAll(s)[0]
 }
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('')
+}
